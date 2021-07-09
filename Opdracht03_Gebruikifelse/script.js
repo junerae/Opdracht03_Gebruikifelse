ec3c9518cf371ce47ba1b7658f6ca2b28d268d4d
junerae <80211043+junerae@users.noreply.github.com>
2021-07-09 12:13:17 +0200

    Add files via upload

---
+++ Opdracht03_Gebruikifelse/script.js
@@ -0,0 +1,27 @@
+const age = 80;
+
+if (age > 90) {
+    console.log("You're too old to come in")
+} else if (age > 18) {
+    console.log("Welcome! You're older than 18 years, so you are allowed to come in")
+} else if (age < 18) {
+    console.log("Unfortunately peanutbutter, you're too young! Come see us again on your 18th birthday!")
+}
+
+
+const isFemale = true;
+
+if (isFemale) {
+    console.log("Welcome to the ladies night! ")
+} else {
+    console.log("Sorry this date is a ladies night, we hope to see you another time!")
+}
+
+
+const driverStatus = 'bob';
+if (driverStatus) {
+    console.log("You're allowed to drive")
+} else {
+    console.log("You're NOT allowed to drive")
+}
+
